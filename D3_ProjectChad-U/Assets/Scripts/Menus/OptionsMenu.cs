5cb355439414d31e0462ebf847d483bfc139fdec
João Prado <59054486+WinterDP@users.noreply.github.com>
2022-09-17 14:34:06 -0300

    Add settings menu
    - add back to menu button
    - volume slider which sets main mixer volume

--- D3_ProjectChad-U/Assets/Scripts/Menus/OptionsMenu.cs
+++ D3_ProjectChad-U/Assets/Scripts/Menus/OptionsMenu.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour
 {
+    [SerializeField] private AudioMixer mainAudioMixer;
 
-
+    public void setVolume(float volume){
+        mainAudioMixer.SetFloat("MainVolume", volume);
+        
+    }
 
     public void BackToMenu()
     {
