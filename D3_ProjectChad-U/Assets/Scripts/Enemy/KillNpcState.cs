6ad5dc3c6fecb6dc9badf4fad10a642c10a338b0
MatheusCapucho <matheusfcapucho@gmail.com>
2022-09-21 22:14:55 -0300

    Timer & Monster

--- D3_ProjectChad-U/Assets/Scripts/Enemy/KillNpcState.cs
+++ D3_ProjectChad-U/Assets/Scripts/Enemy/KillNpcState.cs
@@ -8,7 +8,7 @@ public class KillNpcState : BaseState
     private Animator anim;
 
     private float timer = 0f;
-    private float endTimer = 2f;
+    private float endTimer = 4f;
 
     private ClassroomHolder classroomHolder;
 
@@ -21,7 +21,9 @@ public class KillNpcState : BaseState
     }
     public override Type Tick()
     {
-
+        if (CheckFOV())
+            return typeof(ChaseState);
+       
         timer += Time.deltaTime;
         return Attack();
 
@@ -32,16 +34,47 @@ public class KillNpcState : BaseState
        
 
         if (timer < endTimer)
+        {
+            //AtackAnim - trigger 1x
             return typeof(KillNpcState);
+        }
         else
         {
             timer = 0f;
-            Debug.Log("Attack");
+            Debug.Log("Destroyed");
+            enemy.Target.gameObject.SetActive(false);
+            enemy.SetTarget(null);
+
             EndingManager.Instance.NPCKilled();
             classroomHolder.SetNPC();
             return typeof(SearchState);
         }
 
+    }
+
+
+    private bool CheckFOV()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, enemy.detectionRadius, enemy.targetMask);
+
+        if (colliders.Length > 0)
+        {
+            Transform player = colliders[0].transform;
+            Vector3 dir = (player.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, dir) < enemy.detectionAngle / 2)
+            {
+                float distance = Vector3.Distance(transform.position, player.position);
+
+                if (!Physics.Raycast(transform.position, dir, distance, enemy.wallMask))
+                {
+                    enemy.SetTarget(player);
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
 }
---
+++ D3_ProjectChad-U/Assets/Scripts/SurvivalTime.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SurvivalTime : MonoBehaviour
+{
+    [SerializeField]
+    private float survivalTimer = 75f;
+    private float timer;
+
+    [SerializeField]
+    private TextMeshProUGUI textTimerComponent;
+
+    private GameObject monster;
+
+    void Start()
+    {
+        timer = survivalTimer;
+        monster = GameObject.FindGameObjectWithTag("Enemy");
+    }
+    private void OnEnable()
+    {
+        textTimerComponent.gameObject.SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        monster.SetActive(false);
+        textTimerComponent.gameObject.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        if (timer > 0f)
+        {
+            timer -= Time.fixedDeltaTime;
+            UpdateUI();
+           
+            if (IsTimerComplete())
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private bool IsTimerComplete()
+    {
+        return (timer <= 0f);
+    }
+
+    private void UpdateUI()
+    {
+        string str;
+        int min, sec;
+        sec = (int)timer;
+        min = 0;
+        if (timer >= 60f)
+        {
+            min = (int)timer / 60;
+            sec = (int)timer % 60;
+        }
+        if (sec < 10)
+            str = "" + min + ":0" + sec;
+        else
+            str = "" + min + ":" + sec;
+
+        textTimerComponent.text = str;
+    }
+
+}
